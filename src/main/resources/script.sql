e35a2eaf35920f538d0b8c8bed681c2d88a3d802
Artiom Karasiov <artKarasiov@gmail.com>
2017-02-16 18:50:25 +0300

    Finish sql script

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -13,13 +13,30 @@ CREATE TABLE auto(
   PRIMARY KEY (auto_id)
 );
 
+CREATE TABLE client_data(
+  client_data_id INT NOT NULL AUTO_INCREMENT,
+  passportNumber VARCHAR (10) NOT NULL UNIQUE ,
+  first_name VARCHAR(45) NOT NULL,
+  last_name VARCHAR(45) NOT NULL,
+  mail VARCHAR(100) NOT NULL UNIQUE,
+  PRIMARY KEY (client_data_id)
+);
+
 CREATE TABLE auto_order(
   order_id INT NOT NULL AUTO_INCREMENT,
   start_arend DATETIME,
   finish_arend DATETIME,
   auto_id INT NOT NULL,
   user_id INT NOT NULL,
+  client_data_id INT NOT NULL,
   PRIMARY KEY (order_id),
-  FOREIGN KEY (auto_id) REFERENCES auto(auto_id),
+  FOREIGN KEY (auto_id) REFERENCES auto(auto_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
   FOREIGN KEY (user_id) REFERENCES user(user_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  FOREIGN KEY (client_data_id) REFERENCES client_data(client_data_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
