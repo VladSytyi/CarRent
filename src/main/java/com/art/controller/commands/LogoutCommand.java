42b470475de6cf3d193f00489c292c51c97827e2
Artiom Karasiov <artKarasiov@gmail.com>
2017-02-18 01:41:41 +0300

    Base logout and register logic

--- src/main/java/com/art/controller/commands/LogoutCommand.java
+++ src/main/java/com/art/controller/commands/LogoutCommand.java
@@ -2,11 +2,17 @@ package com.art.controller.commands;
 
 import com.art.controller.FrontCommand;
 
+import javax.servlet.http.HttpSession;
+
 /**
  * Created by user on 18.02.2017.
  */
 public class LogoutCommand extends FrontCommand {
-    public void process() {
+    private HttpSession session;
 
+    public void process() {
+        session = request.getSession();
+        session.setAttribute("userId", null);
+        forward(properties.getProperty("index"));
     }
 }
--- src/main/java/com/art/controller/commands/RegisterCommand.java
+++ src/main/java/com/art/controller/commands/RegisterCommand.java
@@ -1,8 +1,10 @@
 package com.art.controller.commands;
 
 import com.art.controller.FrontCommand;
+import com.art.entity.UserEntity;
 import com.art.service.UserService;
 import com.art.service.impl.UserServiceImpl;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * Created by user on 16.02.2017.
@@ -10,9 +12,21 @@ import com.art.service.impl.UserServiceImpl;
 public class RegisterCommand extends FrontCommand {
 
     private UserService userService;
-
+    private String username;
+    private String password;
 
     public void process() {
+        username = request.getParameter("username");
+        password = request.getParameter("password");
 
+        if(StringUtils.isNotEmpty(username) && StringUtils.isNotEmpty(password)){
+            userService = new UserServiceImpl();
+            userService.registerUser(username, password);
+            /*
+            * TODO: if block for check if user exist
+            * */
+            forward(properties.getProperty("index"));
+        }
+        forward(properties.getProperty("index"));
     }
 }
